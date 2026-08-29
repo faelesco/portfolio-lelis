266cf0be2df2489b2190f455984cab0a11efad77
Raphael Lelis <raphael.lelis@blip.ai>
2026-08-29 13:26:17 -0300

    add header with lights

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
 import { Projects } from "./components/Projects";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-slate-50 dark:bg-slate-950 text-slate-900 dark:text-slate-100 transition-colors">
+      <Header/>
       <Hero
         name="Raphael Lelis"
         title="Tech Lead / Full Stack"
