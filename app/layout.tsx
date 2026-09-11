95b5a5bd1a47d4e3a96d8e68bc93055104176e62
Raphael Lelis <raphael.lelis@blip.ai>
2026-09-11 18:28:25 -0300

    add preview apps

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className="dark">
-      <body className="bg-zinc-100 dark:bg-zinc-950 text-zinc-900 dark:text-zinc-100 selection:bg-emerald-800 selection:text-white transition-colors antialiased min-h-screen flex flex-col">
+      <body className="text-zinc-900 dark:text-zinc-100 selection:bg-emerald-800 selection:text-white transition-colors antialiased min-h-screen flex flex-col relative">
+        <div className="fixed inset-0 -z-10 h-full w-full bg-zinc-100 dark:bg-zinc-950 bg-[radial-gradient(#d4d4d8_1.5px,transparent_1.5px)] dark:bg-[radial-gradient(#27272a_1.5px,transparent_1.5px)] [background-size:24px_24px]"></div>
         <Header />
         <div className="flex-1">
           {children}
